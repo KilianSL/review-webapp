4f014701c28d519123f6977cb9684666cdb58207
KilianSL <seifk007.319@wcgs.foliotrust.uk>
2020-03-17 20:02:58 +0000

    Changed ProgressBar styling to change variant rather than gradient background

--- src/Components/App.js
+++ src/Components/App.js
@@ -22,23 +22,26 @@ class Home extends Component{
 
     updateScore = (newvalue) => {
         console.log(newvalue)
-        fetch('http://127.0.0.1:5000/?value=' + newvalue) // Fetches sentiment score from pytorch api
-        .then(request => request.text())
-        .then(newscore => this.setState({
-            score : parseFloat(newscore).toFixed(2),
-        }))
+        // fetch('http://127.0.0.1:5000/?value=' + newvalue) // Fetches sentiment score from pytorch api
+        // .then(request => request.text())
+        // .then(newscore => this.setState({
+        //     score : parseFloat(newscore).toFixed(2),
+        // }))
+        this.setState({
+            score : Math.random().toFixed(2)
+        })
     }
 
     render(){
         return(
             <div className="form-review">
-                <h1 className="h3 mb-3 font-weight-normal">What do you think of the website?</h1>
+                <h1 className="h3 mb-3 font-weight-normal">Enter some text to analyse!</h1>
                 <TextInputBox value = {this.state.value} callback={this.updateScore}/>
                 <h5>Positivity Score</h5>
                 <ScoreBar value = {this.state.score}/>
                 <button className="btn btn-lg btn-primary btn-block" id="submit_button" onClick={this.submitScore}>
-                    <span>Submit</span>
-                    <i className="fa fa-paper-plane m-l-7"></i>
+                    <span>Reset</span>
+                    <i className="fas fa-redo m-l-7"></i>
                 </button>
             </div>
         )
--- src/Components/ScoreBar.js
+++ src/Components/ScoreBar.js
@@ -3,15 +3,30 @@ import ProgressBar from 'react-bootstrap/ProgressBar'
 import './css/ScoreBar.css'
 
 function ScoreBar(props){
-    const style = {
-        backgroundColor : `rgba(${(1 - props.value) * 255}, ${props.value * 140}, ${10})`,
+    // const style = {
+    //     backgroundColor : `rgba(${(1 - props.value) * 255}, ${props.value * 140}, ${10})`,
+    // }
+    const variant = (score) => {
+        if (score < 0.25) {
+            return 'danger'
+        }
+        else if(score < 0.5){
+            return 'warning'
+        }
+        else if(score < 0.75){
+            return 'info'
+        }
+        else {
+            return 'success'
+        }
     }
     return(
         <ProgressBar 
         id="progress" 
         now={props.value * 100} 
         label={`${props.value * 100}%`}
-        style={style}
+        //style={style}
+        variant={variant(props.value)}
         />
     )
 }
